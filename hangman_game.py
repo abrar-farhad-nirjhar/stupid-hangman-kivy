9467f65efd966b17163e0396d3aab29ea14cfc75
Abrar Farhad <abrar.farhad.af@gmail.com>
2019-01-22 21:10:41 +0600

    Making the app

---
+++ hangman_game.py
@@ -0,0 +1,54 @@
+import linecache
+import random
+
+f = open('words.txt', 'r')
+number_of_words = 0
+for i in f:
+    number_of_words+=1
+
+
+
+
+
+
+
+def punishment(i):
+    if i==1:
+        print(" O ")
+    
+    elif i==2:
+        print(" O ")
+        print("/")
+
+    elif i==3:
+        print(" O ")
+        print("/|")
+    elif i==4:
+        print(" O ")
+        print("/|\\")
+    elif i==5:
+        
+        print(" O ")
+        print("/|\\")
+        print("/")
+    elif i==6:
+        
+        print(" O ")
+        print("/|\\")
+        print("/ \\")
+
+
+def get_word():
+    r = random.randint(0,number_of_words)
+    return linecache.getline("words.txt",r)
+
+
+def game():
+    word = get_word()
+    print(word[0])
+    letters = list('_')*len(word)
+    print(letters)
+
+
+
+game() 
---
+++ main.py
@@ -0,0 +1,42 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+import hangman_game
+
+
+
+class MyImage(Image):
+    def __init__(self, **kwargs):
+        super(MyImage,self).__init__(**kwargs)
+
+
+
+class MyLabel(Label):
+    def __init__(self, **kwargs):
+        super(MyLabel,self).__init__(**kwargs)
+    
+
+
+class mainApp(App):
+    i1 = Image(source='test.jpg')
+    
+    word = hangman_game.get_word()
+    m1 = MyLabel
+    m1.text = "_ "*len(word)
+    # m1.font_size = 20
+
+    def set_size(self):
+        if len(self.word)>10:
+            self.m1.font_size  = 17
+        else:
+            self.m1.font_size = 32
+    
+    def build(self):
+        self.set_size()
+        return GridLayout()
+
+
+# self.MyLabel.setText("hello")
+m = mainApp()
+m.run()
